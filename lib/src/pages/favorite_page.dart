537e5dae0eaedce2b9eb59d2902ba9aff82f5c31
nurtaims <nurtaims@gmail.com>
2019-08-04 11:59:17 +0600

    Testing state of page

--- lib/src/pages/favorite_page.dart
+++ lib/src/pages/favorite_page.dart
@@ -8,6 +8,7 @@ class FavoritePage extends StatefulWidget {
 }
 
 class _FavoritePageState extends State<FavoritePage> {
+  TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +17,27 @@ class _FavoritePageState extends State<FavoritePage> {
         backgroundColor: widget.color,
       ),
       backgroundColor: widget.color,
-      body: Container(),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(new FocusNode());
+        },
+        child: Center(
+          child: Container(
+            width: 300,
+            padding: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+            child: TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.only(bottom: 7, top: 10),
+              ),
+              style: TextStyle(fontSize: 19),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
